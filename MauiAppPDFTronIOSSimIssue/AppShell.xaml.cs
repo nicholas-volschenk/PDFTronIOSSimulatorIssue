c8c70fcb9aa9604a51609daf753322c33577600b
Nicholas Volschenk <nickv@bumblebee.co.za>
2024-05-10 03:09:29 +0200

    Initial Commit

---
+++ MauiAppPDFTronIOSSimIssue/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppPDFTronIOSSimIssue
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
